44cb85a435c895c5b76a4eeae236f801ef58482f
Programmatore in Camicia <programmatoreincamicia@gmail.com>
2026-07-21 17:42:28 +0200

    continued to validation method

--- packages/schema/src/validate.ts
+++ packages/schema/src/validate.ts
@@ -61,16 +61,85 @@ function validateNode(node: unknown, path: string, issues: ValidationIssue[]): v
                 issues.push({ message: 'The stack node must contain children property', path });
             } else {
                 node.children.forEach((element, i) => {
-                    validateNode(element, path + `.children[${i}]`, issues);
+                    validateNode(element, `${path}.children[${i}]`, issues);
                 });
             }
             break;
-    
+        case 'repeat':
+            if (typeof node.dataSource !== 'string') {
+                issues.push({ message: 'The repeat node must contain dataSource property', path });
+            }
+            if (node.template == null) {
+                issues.push({ message: 'The repeat node must contain template property', path });
+            } else {
+                validateNode(node.template, `${path}.template`, issues);
+            }                
+            break;
+        case 'group':
+            if (typeof node.dataSource !== 'string') {
+                issues.push({ message: 'The group node must contain dataSource property', path });
+            }
+            if (typeof node.groupBy !== 'string') {
+                issues.push({ message: 'The group node must contain groupBy property', path });
+            }
+            if (node.detail == null) {
+                issues.push({ message: 'The group node must contain detail property', path });
+            } else {
+                validateNode(node.detail, `${path}.detail`, issues);
+            }
+            if (node.groupHeader != null) {
+                validateNode(node.groupHeader, `${path}.groupHeader`, issues);
+            }
+            if (node.groupFooter != null) {
+                validateNode(node.groupFooter, `${path}.groupFooter`, issues);
+            }
+            break;
+        case 'canvas':
+            if (typeof node.height !== 'string') {
+                issues.push({ message: 'The canvas node must contain height property', path });
+            }
+            if (!Array.isArray(node.children)) {
+                issues.push({ message: 'The canvas node must contain children property', path });
+            } else {
+                node.children.forEach((element, i) => {
+                    if (!isObject(element)) { 
+                        issues.push({ message: 'The canvas children must be a js object', path }); 
+                        return;
+                    }
+                    if (typeof element.x !== 'string') {
+                        issues.push({ 
+                            message: 'The canvas children must contain x property', 
+                            path: `${path}.children[${i}]` 
+                        });
+                    }
+                    if (typeof element.y !== 'string') {
+                        issues.push({ 
+                            message: 'The canvas children must contain y property', 
+                            path: `${path}.children[${i}]` 
+                        });
+                    }
+                    validateNode(element.node, `${path}.children[${i}].node`, issues);
+                });
+            }
+            break;
+        case 'field':
+            if (typeof node.bind !== 'string') {
+                issues.push({ message: 'The field node must contain bind property', path });
+            }
+            break;
+        case 'text':
+            if (typeof node.value !== 'string') {
+                issues.push({ message: 'The text node must contain value property', path });
+            }
+            break;
+        case 'image':
+            if ((typeof node.src !== 'string') && (typeof node.bind !== 'string')) {
+                issues.push({ message: 'The image node must contain src or bind property', path });
+            }
+            break;
         default:
             break;
     }
-
-
 }
 
 function isObject(v: unknown): v is Record<string, unknown> {
