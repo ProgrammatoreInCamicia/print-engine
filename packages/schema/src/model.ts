108a1b0fccbfb4f355906577a328155b3e159c78
ProgrammatoreInCamicia <programamtoreincamicia@gmail.com>
2026-07-21 16:11:16 +0200

    defined all models

--- packages/schema/src/model.ts
+++ packages/schema/src/model.ts
@@ -23,7 +23,75 @@ export interface StackNode {
     children: Node[];
 }
 
+export interface RepeatNode {
+    type: "repeat";
+    dataSource: Expr;
+    template: Node;
+}
+
+export interface GroupNode {
+    type: "group";
+    dataSource: Expr;
+    groupBy: Expr;
+    detail: Node;
+    groupHeader?: Node;
+    groupFooter?: Node;
+}
+
+interface ImageBase {
+    type: "image";
+    width?: Length;
+    height?: Length;
+}
+
+interface ImageSrc extends ImageBase {
+    src: string;
+}
+
+interface ImageBind extends ImageBase {
+    bind: Expr;
+}
+
+export type ImageNode = ImageSrc | ImageBind;
+
+export interface PositionedChild {
+    x: Length;
+    y: Length;
+    w?: Length;
+    h?: Length;
+    node: Node;
+}
+
+export interface CanvasNode {
+    type: "canvas";
+    width?: Length;
+    height: Length;
+    children: PositionedChild[];
+}
+
 export type Node =
   | TextNode
   | FieldNode
-  | StackNode;
+  | StackNode
+  | RepeatNode
+  | GroupNode
+  | ImageNode
+  | CanvasNode;
+
+export interface PageSetup {
+    size: "A4" | "A5" | "Letter" | "Legal";
+    orientation?: "portrait" | "landscape";
+    margin?: Length;
+}
+
+export interface Regions {
+    header?: Node;
+    footer?: Node;
+}
+
+export interface PrintDocument {
+    schemaVersion: number;
+    page: PageSetup;
+    body: Node;
+    regions?: Regions;
+}
